05dde85692d9600453abdbc653978a582695d9e8
jjkazm <jjkazm@gmail.com>
2019-01-16 14:03:22 +0100

    Add and test validations to article model

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -12,6 +12,22 @@ RSpec.feature "Creating article" do
     click_button "Create Article"
 
     expect(page).to have_content("Article has been created")
-    expect(page.current_path).to eq(articles_path)    
+    expect(page.current_path).to eq(articles_path)
   end
+
+  scenario "user fails with empty title and body" do
+     visit "/"
+
+     click_link "New Article"
+
+     fill_in "Title", with: ""
+     fill_in "Body", with: ""
+
+     click_button "Create Article"
+
+     expect(page).to have_content("Article has not been created")
+     expect(page).to have_content("Title can't be blank")
+     expect(page).to have_content("Body can't be blank")
+  end
+
 end
