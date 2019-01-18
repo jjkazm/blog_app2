47bfb155ae4253f982c6ad13b3adddb0325750eb
jjkazm <jjkazm@gmail.com>
2019-01-18 12:45:54 +0100

    Style login page

--- spec/features/signing_in_user_spec.rb
+++ spec/features/signing_in_user_spec.rb
@@ -11,12 +11,27 @@ RSpec.feature 'Users signin' do
 
     fill_in "Email", with: "kuba@wp.pl"
     fill_in "Password", with: "haslo123"
-    click_button "Login"
+    click_button "Log in"
 
-    expect(page).to have_content("Signed in succesfully.")
+    expect(page).to have_content("Signed in successfully.")
     expect(page).to have_content("Signed in as #{@user.email}")
     expect(page).not_to have_link("Login")
     expect(page).not_to have_link("Signup")
-    expect(page.current_path).to eq(articles_path)
+    expect(page.current_path).to eq(root_path)
+  end
+
+  scenario 'User fails sign in with invalid credentials' do
+    visit '/'
+    click_link "Login"
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    click_button "Log in"
+
+    expect(page).to have_content("Invalid Email or password.")
+    expect(page).not_to have_content("Signed in as #{@user.email}")
+    expect(page).to have_link("Login")
+    expect(page).to have_link("Signup")
+    expect(page.current_path).to eq(new_user_session_path)
   end
 end
