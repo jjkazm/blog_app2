24f802418b882ccc369f8e0d6a2e50e0f28f3d35
jjkazm <jjkazm@gmail.com>
2019-01-21 21:41:03 +0100

    Hide edit and delete article button form not owner users

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.feature "Show article" do
 
   before do
-    adam = User.create(email: "adam@wp.pl", password: "haslo123")
-    sign_in adam
-    @article = Article.create(title: "First Article", body: "This is body of the first article", user: adam)
+    @adam = User.create(email: "adam@wp.pl", password: "haslo123")
+    @jurek = User.create(email: "jurek@wp.pl", password: "haslo123")
+    @article = Article.create(title: "First Article", body: "This is body of the first article", user: @adam)
   end
 
-  scenario "User shows an article" do
+  scenario "to non-signed user" do
     visit "/"
 
     click_link @article.title
@@ -16,5 +16,34 @@ RSpec.feature "Show article" do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
+
+  scenario "when signed user is author of the post" do
+    sign_in @adam
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
+
+  scenario "when signed user is not author of the post" do
+    sign_in @jurek
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
 end
