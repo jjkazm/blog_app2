420497dde8404c375fef4aafe9f5f6d6710337a2
jjkazm <jjkazm@gmail.com>
2019-01-21 17:46:32 +0100

    Fix 'show article' spec

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 RSpec.feature "Show article" do
 
   before do
-    @article = Article.create(title: "First Article", body: "This is body of the first article")
+    adam = User.create(email: "adam@wp.pl", password: "haslo123")
+    sign_in adam
+    @article = Article.create(title: "First Article", body: "This is body of the first article", user: adam)
   end
 
   scenario "User shows an article" do
